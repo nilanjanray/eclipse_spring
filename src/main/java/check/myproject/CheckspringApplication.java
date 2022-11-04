bd33210d70b52e60b8295ef87dc5be5df756e2f8
Nilanjan Roy <nilanjanroy@Nilanjans-MacBook-Pro.local>
2022-11-04 20:31:04 +0530

    inititial commit

---
+++ src/main/java/check/myproject/CheckspringApplication.java
@@ -0,0 +1,13 @@
+package check.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/check/myproject/CheckspringApplicationTests.java
@@ -0,0 +1,13 @@
+package check.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
