1e951e0c19474bd5e2452d6db74636888c16b5b0
Nilanjan Roy <nilanjanroy@Nilanjans-MacBook-Pro.local>
2022-11-06 11:15:21 +0530

    Changes commited

--- src/main/java/check/myproject/controllers/FirstController.java
+++ src/main/java/check/myproject/controllers/FirstController.java
@@ -1,17 +1,17 @@
 package check.myproject.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import check.spring.microservice.lib.Mylib;
+
+@RestController
 public class FirstController {
 
-	public int FirstFunction() {
-		return 1;
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 	
-	public double SecondFunction(){
-		return 1;
-	}
 	
-	public String ThridFunction() {
-		return "String";
-	}
 		
 }
---
+++ src/main/java/check/spring/microservice/lib/Mylib.java
@@ -0,0 +1,5 @@
+package check.spring.microservice.lib;
+
+public class Mylib {
+
+}
